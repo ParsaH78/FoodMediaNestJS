b66e0f284358d9f6ae926d0cbc1828bc5cf5c4e0
Parsa Hosni <parsa.hsn78@gmail.com>
2023-01-27 20:20:25 +0330

    stuff updated

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import auth from './AuthReducer';
-import user from './UserReducer';
-import post from './PostReducer';
-import chat from './ChatReducer';
-
-
-export const reducers = combineReducers({ auth, user, post, chat });
